edd19416f4442877dbeb20321457974262b63ad6
Parsa <mehdipourparsa@gmail.com>
2023-12-03 11:00:24 +0330

    feat : TaxCalculatorService

---
+++ DemoLibirary/Services/TaxCalculatorService.cs
@@ -0,0 +1,22 @@
+﻿using DemoLibrary.Entity;
+using DemoLibrary.Infra;
+using DemoLibrary.Interface;
+
+namespace DemoLibrary.Services
+{
+    public class TaxCalculatorService
+    {
+        public TaxPayer CalculateTaxPercentage(TaxPayer taxPayer)
+        {
+            var ruleType = typeof(ITaxRule);
+            IEnumerable<ITaxRule?> rules = this.GetType().Assembly.GetTypes()
+                .Where(p => ruleType.IsAssignableFrom(p) && !p.IsInterface)
+                .Select(r => Activator.CreateInstance(r) as ITaxRule);
+
+            var engine = new TaxRuleEngine(rules);
+
+            return engine.CalculateTaxPercentage(taxPayer);
+        }
+    }
+}
+
