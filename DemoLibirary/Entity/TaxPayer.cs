c3654f5393d16acbab181f004aea5b320eca3568
Parsa <mehdipourparsa@gmail.com>
2023-12-03 10:56:06 +0330

    feat : Enitity

---
+++ DemoLibirary/Entity/TaxPayer.cs
@@ -0,0 +1,15 @@
+﻿namespace DemoLibrary.Entity
+{
+    public class TaxPayer
+    {
+        public double GrossIncome { get; set; }
+        public bool IsSingle { get; set; }
+        public bool IsRetired { get; set; }
+        public bool IsResidentOrCitizen { get; set; }
+        public bool HasHealthInsurance { get; set; }
+        public bool HasBusiness { get; set; }
+        public bool AtLossInBusiness { get; set; }
+        public double TaxedAmount { get; set; }
+        public double HealthInsuranceAnnualPremium { get; set; }
+    }
+}
