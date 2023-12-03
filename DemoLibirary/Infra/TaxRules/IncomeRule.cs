aa8e42c16ea4952606b57fb991fe593db5cba4ed
Parsa <mehdipourparsa@gmail.com>
2023-12-03 10:59:48 +0330

    feat : TaxRules

---
+++ DemoLibirary/Infra/TaxRules/IncomeRule.cs
@@ -0,0 +1,18 @@
+﻿using DemoLibrary.Entity;
+using DemoLibrary.Interface;
+
+namespace DemoLibrary.Infra.TaxRules
+{
+    public class IncomeRule : ITaxRule
+    {
+        public TaxPayer CalculateTaxPercentage(TaxPayer taxPayer, double currentPercentage)
+        {
+            if (taxPayer.GrossIncome < 40000) taxPayer.TaxedAmount = 0;
+            else
+            {
+                taxPayer.TaxedAmount += ((taxPayer.GrossIncome - 40000) * .1);
+            }
+            return taxPayer;
+        }
+    }
+}
---
+++ DemoLibirary/Infra/TaxRules/SingleRule.cs
@@ -0,0 +1,15 @@
+﻿using DemoLibrary.Entity;
+using DemoLibrary.Interface;
+
+namespace DemoLibrary.Infra.TaxRules
+{
+    public class SingleRule : ITaxRule
+    {
+        public TaxPayer CalculateTaxPercentage(TaxPayer taxPayer, double currentPercentage)
+        {
+            if (taxPayer.IsSingle)
+                taxPayer.TaxedAmount += ((taxPayer.GrossIncome - 40000) * .1);
+            return taxPayer;
+        }
+    }
+}
