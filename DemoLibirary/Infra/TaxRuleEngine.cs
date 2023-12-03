83881f5bbe7677e920855d3276da8b5be1a999b2
Parsa <mehdipourparsa@gmail.com>
2023-12-03 11:00:12 +0330

    feat : TaxRuleEngine

---
+++ DemoLibirary/Infra/TaxRuleEngine.cs
@@ -0,0 +1,24 @@
+﻿using DemoLibrary.Entity;
+using DemoLibrary.Interface;
+
+namespace DemoLibrary.Infra
+{
+    public class TaxRuleEngine
+    {
+        private readonly List<ITaxRule> _rules = new();
+
+        public TaxRuleEngine(IEnumerable<ITaxRule?> rules)
+        {
+            _rules.AddRange(rules);
+        }
+
+        public TaxPayer CalculateTaxPercentage(TaxPayer taxPayer)
+        {
+            foreach (var rule in _rules)
+            {
+                taxPayer.TaxedAmount += rule.CalculateTaxPercentage(taxPayer, taxPayer.TaxedAmount).TaxedAmount;
+            }
+            return taxPayer;
+        }
+    }
+}
